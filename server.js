74f507b4a00999fb741c36453c238b2ff1b0848a
Gijs Schouten <21969@ma-web.nl>
2018-03-21 14:55:31 +0100

    Git Gut

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const io = require('socket.io')(http);
 
 var Json = '[{"x":0,"y":0,"color":2,"colorBit":false},{"x":100,"y":0,"color":15,"colorBit":false},{"x":200,"y":0,"color":15,"colorBit":false},{"x":300,"y":0,"color":15,"colorBit":false},{"x":400,"y":0,"color":2,"colorBit":false},{"x":500,"y":0,"color":15,"colorBit":false},{"x":600,"y":0,"color":15,"colorBit":false},{"x":700,"y":0,"color":15,"colorBit":false},{"x":0,"y":100,"color":15,"colorBit":false},{"x":100,"y":100,"color":2,"colorBit":false},{"x":200,"y":100,"color":15,"colorBit":false},{"x":300,"y":100,"color":2,"colorBit":false},{"x":400,"y":100,"color":15,"colorBit":false},{"x":500,"y":100,"color":15,"colorBit":false},{"x":600,"y":100,"color":15,"colorBit":false},{"x":700,"y":100,"color":15,"colorBit":false},{"x":0,"y":200,"color":15,"colorBit":false},{"x":100,"y":200,"color":15,"colorBit":false},{"x":200,"y":200,"color":2,"colorBit":false},{"x":300,"y":200,"color":15,"colorBit":false},{"x":400,"y":200,"color":15,"colorBit":false},{"x":500,"y":200,"color":15,"colorBit":false},{"x":600,"y":200,"color":15,"colorBit":false},{"x":700,"y":200,"color":15,"colorBit":false},{"x":0,"y":300,"color":15,"colorBit":false},{"x":100,"y":300,"color":15,"colorBit":false},{"x":200,"y":300,"color":2,"colorBit":false},{"x":300,"y":300,"color":15,"colorBit":false},{"x":400,"y":300,"color":15,"colorBit":false},{"x":500,"y":300,"color":15,"colorBit":false},{"x":600,"y":300,"color":15,"colorBit":false},{"x":700,"y":300,"color":15,"colorBit":false},{"x":0,"y":400,"color":15,"colorBit":false},{"x":100,"y":400,"color":2,"colorBit":false},{"x":200,"y":400,"color":2,"colorBit":false},{"x":300,"y":400,"color":2,"colorBit":false},{"x":400,"y":400,"color":15,"colorBit":false},{"x":500,"y":400,"color":15,"colorBit":false},{"x":600,"y":400,"color":15,"colorBit":false},{"x":700,"y":400,"color":15,"colorBit":false},{"x":0,"y":500,"color":15,"colorBit":false},{"x":100,"y":500,"color":2,"colorBit":false},{"x":200,"y":500,"color":15,"colorBit":false},{"x":300,"y":500,"color":2,"colorBit":false},{"x":400,"y":500,"color":15,"colorBit":false},{"x":500,"y":500,"color":15,"colorBit":false},{"x":600,"y":500,"color":15,"colorBit":false},{"x":700,"y":500,"color":15,"colorBit":false},{"x":0,"y":600,"color":15,"colorBit":false},{"x":100,"y":600,"color":2,"colorBit":false},{"x":200,"y":600,"color":15,"colorBit":false},{"x":300,"y":600,"color":2,"colorBit":false},{"x":400,"y":600,"color":15,"colorBit":false},{"x":500,"y":600,"color":15,"colorBit":false},{"x":600,"y":600,"color":15,"colorBit":false},{"x":700,"y":600,"color":15,"colorBit":false},{"x":0,"y":700,"color":15,"colorBit":false},{"x":100,"y":700,"color":15,"colorBit":false},{"x":200,"y":700,"color":15,"colorBit":false},{"x":300,"y":700,"color":15,"colorBit":false},{"x":400,"y":700,"color":15,"colorBit":false},{"x":500,"y":700,"color":15,"colorBit":false},{"x":600,"y":700,"color":15,"colorBit":false},{"x":700,"y":700,"color":15,"colorBit":false}]';
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
@@ -23,6 +25,6 @@ io.on('connection', function(socket){
     })
 });
 
-http.listen(3000, function(){
+http.listen(port, function(){
     console.log('listening on *:3000');
 });
